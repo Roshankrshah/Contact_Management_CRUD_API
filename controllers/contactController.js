21b928bd6012d1b2eddb48c306cdd9c427579225
Roshan <shahroshan0720@gmail.com>
2023-06-16 14:14:09 +0530

    setup errorhandler and database

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,21 +1,29 @@
-const getContacts = (req,res) => {
+const asyncHandler = require('express-async-handler');
+
+const getContacts = asyncHandler((req,res) => {
     res.send("you are at contact pages");
-};
+});
 
-const createContact = (req,res) => {
-    res.send("contact pages create with help of");
-};
+const createContact = asyncHandler((req,res) => {
+    const {name, email, phone} = req.body;
+    if(!name||!email||!phone){
+        res.status(400);
+        throw new Error("All field are mandatory");
+    }
+    console.log(req.body);
+    res.send(`contact pages create with help of`);
+});
 
-const getContact = (req,res) => {
+const getContact = asyncHandler((req,res) => {
     res.send(`contact of ${req.params.id}`);
-};
+});
 
-const updateContact = (req,res) => {
+const updateContact = asyncHandler((req,res) => {
     res.send(`contact update of ${req.params.id}`);
-}
+});
 
-const deleteContact = (req,res) => {
+const deleteContact = asyncHandler((req,res) => {
     res.send(`delete contact of ${req.params.id}`);
-};
+});
 
 module.exports = {getContacts,createContact,getContact,updateContact,deleteContact};
--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorhandler');
+const connectDb = require('./config/dbConnection');
+const dotenv = require('dotenv').config();
+
+connectDb();
 const app = express();
+
 
 port = process.env.PORT || 5000;
 
+
+app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
+app.use(errorHandler);
+
 
 app.listen(port, () =>{
     console.log(`Server listening at http://localhost:${port}`);
