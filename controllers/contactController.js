46537b145f6972bdb56fea9d3d0240283b763116
Roshan <shahroshan0720@gmail.com>
2023-06-16 15:02:35 +0530

    setup crud api

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,29 +1,64 @@
 const asyncHandler = require('express-async-handler');
+const Contact = require('../models/contactModel')
 
-const getContacts = asyncHandler((req,res) => {
-    res.send("you are at contact pages");
+const getContacts = asyncHandler(async (req,res) => {
+    const contacts = await Contact.find({});
+    res.status(200).json(contacts);
 });
 
-const createContact = asyncHandler((req,res) => {
-    const {name, email, phone} = req.body;
-    if(!name||!email||!phone){
-        res.status(400);
-        throw new Error("All field are mandatory");
+const createContact = asyncHandler(async (req,res) => {
+    console.log("The request body is :", req.body);
+    const { name, email, phone } = req.body;
+    if (!name || !email || !phone) {
+      res.status(400);
+      throw new Error("All fields are mandatory !");
     }
-    console.log(req.body);
-    res.send(`contact pages create with help of`);
+    const contact = await Contact.create({
+        name,
+        email,
+        phone,
+      });
+    
+      res.status(201).json(contact);
 });
 
-const getContact = asyncHandler((req,res) => {
-    res.send(`contact of ${req.params.id}`);
+const getContact = asyncHandler(async (req,res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if(!contact){
+        req.status(404);
+        throw new Error("Contact not Found");
+    }
+
+    res.status(200).json(contact);
 });
 
-const updateContact = asyncHandler((req,res) => {
-    res.send(`contact update of ${req.params.id}`);
+const updateContact = asyncHandler(async (req,res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if(!contact){
+        req.status(404);
+        throw new Error("Contact not Found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new :true}
+    )
+
+    res.status(200).json(updatedContact);
 });
 
-const deleteContact = asyncHandler((req,res) => {
-    res.send(`delete contact of ${req.params.id}`);
+const deleteContact = asyncHandler(async (req,res) => {
+    const contact = await Contact.findById(req.params.id);
+
+    if(!contact){
+        req.status(404);
+        throw new Error("Contact not Found");
+    }
+    await Contact.Remove();
+    res.status(200).json(contact);
 });
 
 module.exports = {getContacts,createContact,getContact,updateContact,deleteContact};
