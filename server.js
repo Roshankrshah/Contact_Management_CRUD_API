85bb5634167286e430078dbee1c174920dc4df3b
Roshan <shahroshan0720@gmail.com>
2023-06-28 21:52:14 +0530

    not orignal one

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-const express = require('express');
-const errorHandler = require('./middleware/errorhandler');
-const connectDb = require('./config/dbConnection');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const connectDb = require("./config/dbConnection");
+const errorHandler = require("./middleware/errorHandler");
+const dotenv = require("dotenv").config();
 
 connectDb();
 const app = express();
 
-
-port = process.env.PORT || 5000;
-
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/contacts', require('./routes/contactRoutes'));
-app.use('/api/user', require('./routes/userRoutes'));
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
 
-
-app.listen(port, () =>{
-    console.log(`Server listening at http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
